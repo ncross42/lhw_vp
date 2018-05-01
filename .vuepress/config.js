2723624b7e4b3c3bc3ef09ca0507a35fa623c189
HeeWon Lee <ncross42@gmail.com>
2018-05-02 03:44:28 +0900

    add docs for github.io

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   base: '/lhw_vp/',
+  dest: 'docs',
   title: '이희원 서초구의원 예비후보자',
   description: '이희원 서초구의원 가선거구(반포1,3,4,잠원동) 예비후보자',
-  dest: 'docs',
+  ga: 'UA-118543834-1',
   // themeConfig: {
   //   nav: [
   //     {text: '공약', link: '/Commitment'},
--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,7 @@ vuepress build
 
 
 # navigate into the build output directory
-# cd docs/.vuepress/dist
-cd dist
+cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,9 +19,11 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git remote add origin https://github.com/ncross42/ncross42.github.io.git
+git push -u origin master
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ncross42/lhw_vp.git master:gh-pages
+#git push -f git@github.com:ncross42/lhw_vp.git master:gh-pages
 
 cd -
