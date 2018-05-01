027a95b9b27fa46d2002cf603e011d2b89505e2d
HeeWon Lee <ncross42@gmail.com>
2018-05-02 02:48:37 +0900

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  base: '/',
+  // base: '/vuepress/',
+  title: '이희원 서초구의원 예비후보자',
+  description: '이희원 서초구의원 가선거구(반포1,3,4,잠원동) 예비후보자',
+  dest: 'docs',
+  // themeConfig: {
+  //   nav: [
+  //     {text: '공약', link: '/Commitment'},
+  //     {text: '이력', link: '/history'},
+  //     {text: '균형민주주의', link: '/dobalance'},
+  //   ]
+  // }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vuepress
 Vuepress 를 이용한 이희원 개인페이지
+
+# Hello world
